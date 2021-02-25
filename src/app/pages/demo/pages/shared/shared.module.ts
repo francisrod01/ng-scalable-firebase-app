a46fbbdb23b7781935625fc4d196b7b6eef655fb
Francis Rodrigues <francis.rod01@outlook.com>
2021-02-25 20:59:29 -0300

    wip: Added shared input controls component

--- src/app/pages/demo/pages/shared/shared.module.ts
+++ src/app/pages/demo/pages/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedRoutingModule } from './shared-routing.module';
 
 import { SharedComponent } from './shared.component';
 
-import { ButtonModule } from '@app/shared';
+import { ButtonModule, ControlsModule } from '@app/shared';
 
 @NgModule({
   declarations: [SharedComponent],
   imports: [
     CommonModule,
     SharedRoutingModule,
+    ReactiveFormsModule,
     ButtonModule,
+    ControlsModule,
   ]
 })
 export class SharedModule { }
