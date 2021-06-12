885bfbb7afd4306ea95b9eca310b2a80cb4aeda7
Francis Rodrigues <francis.rod01@outlook.com>
2021-06-11 21:57:56 -0300

    wip: Added shared password controls component

--- src/app/shared/controls/controls.module.ts
+++ src/app/shared/controls/controls.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { InputModule } from './input/input.module';
 import { FormFieldModule } from './form-field/form-field.module';
+import { PasswordModule } from './password/password.module';
 
 @NgModule({
   declarations: [],
@@ -10,10 +11,12 @@ import { FormFieldModule } from './form-field/form-field.module';
     CommonModule,
     InputModule,
     FormFieldModule,
+    PasswordModule,
   ],
   exports: [
     InputModule,
     FormFieldModule,
+    PasswordModule,
   ]
 })
 export class ControlsModule { }
